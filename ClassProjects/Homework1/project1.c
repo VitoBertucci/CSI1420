48b275e334f829ef27f7fa44d507cd0830e8f7d8
VitoBertucci <vito.rob.bertucci@gmail.com>
2022-01-22 13:04:51 -0500

    problem 23 done

--- ClassProjects/Homework1/project1.c
+++ ClassProjects/Homework1/project1.c
@@ -3,13 +3,55 @@
 int main(void) {
 //23 - read three ints determines and prints largest and smallest
 
+    //get three ints and store them
+    int num1,num2,num3;
+    printf("Enter first number: \n");
+    scanf("%d", &num1);
+    printf("Enter second number: \n");
+    scanf("%d", &num2);
+    printf("Enter third number: \n");
+    scanf("%d", &num3);
+
+    int large,small;
+    
+    //get large and small of first and second num
+    if(num1 < num2) {
+        small = num1;
+        large = num2;
+    } else if(num1 > num2) { 
+        large = num1;
+        small = num2;
+    } else if(num1 == num2) {
+        large = num1;
+        small = num2;
+    }
+
+    //num3 assignment according to large and small found above
+    if(num3 > large) {
+        large = num3;
+    } else if(num3 < small) {
+        small = num3;
+    }
+
+    //display 
+    if(num1 == num2 && num1 == num3) {
+        printf("All three numbers are the same");
+    } else {
+        printf("large num is: %d\n", large);
+        printf("small num is: %d\n", small);
+    }
+
 //25 - print my initials in block letters down the page, each letter made out of that letter
 
 //29 - print integer equivalents of letters
-    char c;
-    printf("Enter char to get int value for\n");
-    scanf("%c", &c);
-    printf("Int value of char: %d\n", c);
+
+    // //init char
+    // char c;
+    // //ask and store user char
+    // printf("Enter char to get int value for\n");
+    // scanf("%c", &c);
+    // //display char as int value
+    // printf("Int value of char: %d\n", c);
 //32 
 /*
     BMI calculator
