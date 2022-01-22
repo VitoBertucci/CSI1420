f8f1d9d258e32fbcd59ca30019c1c72f840a6f08
VitoBertucci <vito.rob.bertucci@gmail.com>
2022-01-22 12:42:18 -0500

    example 29 done

--- ClassProjects/Homework1/project1.c
+++ ClassProjects/Homework1/project1.c
@@ -1,2 +1,23 @@
 #include <stdio.h>
 
+int main(void) {
+//23 - read three ints determines and prints largest and smallest
+
+//25 - print my initials in block letters down the page, each letter made out of that letter
+
+//29 - print integer equivalents of letters
+    char c;
+    printf("Enter char to get int value for\n");
+    scanf("%c", &c);
+    printf("Int value of char: %d\n", c);
+//32 
+/*
+    BMI calculator
+        take (weight x 703)/ (height)^2
+        display normal BMI values for user:
+            Underweight: under 18.5
+            Normal: 18.5-24.9
+            Overweight: 25-29.9
+            Obese: 30 and over
+*/
+}
