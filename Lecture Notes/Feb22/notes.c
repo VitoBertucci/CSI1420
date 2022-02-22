3a1345ed6accd317a205c20ab041993e5d72d21a
VitoBertucci <vito.rob.bertucci@gmail.com>
2022-02-22 14:43:48 -0500

    notes

--- Lecture Notes/Feb22/notes.c
+++ Lecture Notes/Feb22/notes.c
@@ -5,8 +5,27 @@ compile linked files:
 >> gcc -c File1.c -o File1.o
 >> gcc -c File2.c -o File2.o
 >> gcc File1.o File2.o -o Prog.out
+
+storage class specifiers:
+    -auto
+    -register = faster (stored in cpu)
+    -extern
+    -static
 */
 
+//global variable
+int x = 1;
+
 int main(void) {
+
+    //new scope
+    int x = 5;
+    //printf("%d", x);
+
+    {
+        //inside scope
+        int x = 7;
+        //printf("%d", x);
+    }
 
 }
