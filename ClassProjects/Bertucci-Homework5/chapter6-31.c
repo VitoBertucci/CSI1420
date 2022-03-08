4c606eeeabbf8945c81d6e4856f116cf669a21f3
VitoBertucci <vito.rob.bertucci@gmail.com>
2022-03-08 08:17:09 -0500

    clean

--- ClassProjects/Bertucci-Homework5/chapter6-31.c
+++ ClassProjects/Bertucci-Homework5/chapter6-31.c
@@ -13,18 +13,14 @@ int validChar(char c) {
 
 //remove all spaces and punctuation
 void fixString(char str[]) {
-
     //loop through string
     for (int i = 0, j; str[i] != '\0'; ++i) {
-
         //if current char is invalid and not end char
         while ((validChar(str[i]) == 0) && !(str[i] == '\0')) {
             for (j = i; str[j] != '\0'; ++j) {
-                
                 //change current char to char to right of it
                 str[j] = str[j + 1];
             }
-
             //end string and get size of final array
             str[j] = '\0';
         }
@@ -32,27 +28,21 @@ void fixString(char str[]) {
 }
 
 void palinString(char str[]) {
-
     //set index for first character
     int i = 0;
-
     //set index for last character
     int e = strlen(str) -1;
-
     //loop comparing first and last char, then 2nd and 2nd to last character
     while(e > i) {
-
         //if the two chars ever dont match, return no
         if (str[i] != str[e]) {
             printf("NO\n");
             return;
         }
-
         //increment values
         i++;
         e--;
     }
-
     //if all chars match, return yes
     printf("YES\n");
     return;
@@ -61,21 +51,15 @@ void palinString(char str[]) {
 int main(void) {
     //init string char arr
     char word[] = {0};
-
     //get input string
     scanf("%[^\n]s", word);
-
     //remove spaces and punctuation
     fixString(word);
-
     //output string
     printf("String to test: %s\n", word);
-
     //output palindrome test
     printf("Palinfrome: ");
     palinString(word);
-
     //dont know why segmentation fault at 10 chars in array
-
 }
 
--- ClassProjects/Bertucci-Homework5/chapter6-33.c
+++ ClassProjects/Bertucci-Homework5/chapter6-33.c
@@ -8,30 +8,23 @@ void printRow(int arr[], int a, int mid, int b);
 
 //recursive binary search
 int binarySearch (int arr[], int a, int b, int key) {
-
     //get mid value of current search
     int mid = a + (b-a) / 2;
-
     //print current search range
     printRow(arr, a, mid, b);
-
     //if end value is greater than 1 (if there are enough elements)
     if(b >= a) {
-
         //if key is middle value, return middle value
         if(arr[mid] == key) {
             return mid;
         }
-
         //if key is greater than middle value, lower ceiling value and run again
         if(arr[mid] > key) {
             return binarySearch(arr, a, mid-1, key);
         }
-
         //if key is less than mid value, raise floor and search again
         return binarySearch(arr, mid+1, b, key);
     }
-
     //none of the above conditionals are met, return -1 (val not found)
     return -1;
 }
@@ -39,74 +32,58 @@ int binarySearch (int arr[], int a, int b, int key) {
 //print indicies
 void printHeader(void) {
     printf("\nIndicies:\n");
-
     //loop through array and print all indicies
     for(int i = 0; i < SIZE; i++) {
         printf("%3d ", i);
     }
-
     //new line
     printf("\n");
-
     //print --- line
     for(int i = 0; i <= SIZE*4; i++) {
         printf("-");
     }
-
     //new line
     printf("\n");
 }
 
 //print search row
 void printRow(int arr[], int a, int mid, int b) {
-
     //loop through array
     for(int i = 0; i < SIZE; i++) {
-
         //if i is outside of range, print nothing
         if(i < a || i > b) {
             printf("%s","    ");
         }
-
         //if i is mid value, mark
         else if(i == mid) {
             printf("%3d*", arr[i]);
         }
-
         //print i if in range and not mid value
         else {
             printf("%3d ", arr[i]);
         }
     }
-
     printf("\n");
 }
 
 
 int main(void) {
-
     //init array and args
     int arr[SIZE];
     int key, result;
-
     //fill array
     for(size_t i = 0; i < SIZE; ++i) {
         arr[i] = 2 * i;
     }
-
     //get n number of elements in array
     int n = sizeof(arr) / sizeof(arr[0]);
-
     //prompt a store search key
     printf("Enter key (Between 0 and 28): ");
     scanf("%d", &key);
-
     //print indicies/header
     printHeader();
-
     //run search
     result = binarySearch(arr, 0, n-1, key);
-
     //print result
     if (result != -1) {
         printf("\n%d found at index: %d\n", key, result);
